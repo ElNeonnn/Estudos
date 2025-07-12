8c22f67454e72984cc287bd97c99796aaebca7d9
ElNeonnn <jndrsn.08@gmail.com>
2025-07-12 10:15:51 -0300

    aula 1

---
+++ C Sharp/Aula01/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula01
+{
+    internal class Person
+    {
+        // Campos
+        public string name = String.Empty;
+        public int age;
+
+        // Método sem retorno
+        public void Apresentation()
+        {
+            Console.WriteLine($"Olá! Meu nome é {name} e eu tenho {age} anos.");
+        }
+
+        // Método com retorno
+        public string VerifyAge()
+        {
+            return age >= 18 ? "Esta pessoa é maior de idade." : "Esta pessoa é menor de idade.";
+        }
+    }
+}
---
+++ C Sharp/Aula01/Program.cs
@@ -0,0 +1,29 @@
+﻿using Aula01;
+
+// Exemplo 1
+Person pessoa1 = new Person();
+
+pessoa1.name = "Joanderson";
+pessoa1.age = 16;
+
+pessoa1.Apresentation();
+Console.WriteLine(pessoa1.VerifyAge());
+
+// Exemplo 2
+Person pessoa2 = new();
+
+pessoa2.name = "Carina";
+pessoa2.age = 1400096;
+
+pessoa2.Apresentation();
+Console.WriteLine(pessoa2.VerifyAge());
+
+// Exemplo 3
+Person pessoa3 = new()
+{
+    name = "José",
+    age = 2
+};
+
+pessoa3.Apresentation();
+Console.WriteLine(pessoa3.VerifyAge());
