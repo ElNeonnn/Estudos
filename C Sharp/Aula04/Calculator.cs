06589d59eed1a72d36173fbb33231cdf0c901d43
ElNeonnn <jndrsn.08@gmail.com>
2025-07-12 16:04:45 -0300

    aula 4

---
+++ C Sharp/Aula04/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula04
+{
+    internal class Calculator
+    {
+        // Propriedade numérica
+        public static int Number { get; set; } = 10;
+
+        // Método para somar dois valores
+        public static void Add(int n1, int n2)
+        {
+            Console.WriteLine($"A soma dos valores é: {n1 + n2}");
+        }
+    }
+}
---
+++ C Sharp/Aula04/Program.cs
@@ -0,0 +1,7 @@
+﻿// Exibir o valor contido na propriedade Number
+using Aula04;
+
+Console.WriteLine($"Número: {Calculator.Number}");
+
+// Realizar a soma
+Calculator.Add(10, 25);
