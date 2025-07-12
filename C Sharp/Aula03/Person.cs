eb5f2a577ce6ef9211b11ba8e6a624e4bbe9bfcd
ElNeonnn <jndrsn.08@gmail.com>
2025-07-12 15:56:03 -0300

    aula 3

---
+++ C Sharp/Aula03/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula03
+{
+    internal class Person
+    {
+        // 1º construtor
+        public Person()
+        {
+            Console.WriteLine("Olá, Mundo!");
+        }
+
+        // 2º construtor
+        public Person(string name)
+        {
+            Console.WriteLine($"Olá, {name}!");
+        }
+
+        // 3º construtor
+        public Person(string name, int age)
+        {
+            Console.WriteLine($"Olá, {name}! Você tem {age} anos.");
+        }
+    }
+}
---
+++ C Sharp/Aula03/Program.cs
@@ -0,0 +1,10 @@
+﻿// Criar primeiro objeto
+using Aula03;
+
+Person p1 = new();
+
+// Criar segundo objeto
+Person p2 = new("Carina");
+
+// Criar terceiro objeto
+Person p3 = new("Joanderson", 16);
