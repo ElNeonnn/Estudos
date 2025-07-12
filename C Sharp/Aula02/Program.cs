7d4056503c743bc13d61fe69f2066d5d21c10d64
ElNeonnn <jndrsn.08@gmail.com>
2025-07-12 15:45:17 -0300

    aula 2

---
+++ C Sharp/Aula02/Program.cs
@@ -0,0 +1,8 @@
+﻿using Aula02;
+
+Student std = new();
+
+std.Name = "Joanderson";
+std.Media = 8;
+
+Console.WriteLine($"O(A) aluno(a) {std.Name} obteve a média {std.Media}.");
---
+++ C Sharp/Aula02/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula02
+{
+    internal class Student
+    {
+        // 1ª primeira
+        private string name = String.Empty;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // 2ª maneira
+        public double Media { get; set; }
+    }
+}
