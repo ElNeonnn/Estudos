6a79d2df7fbbf917549d898375c0a523bb7c0126
ElNeonnn <jndrsn.08@gmail.com>
2025-07-13 11:46:08 -0300

    aula 12

---
+++ C Sharp/Aula12/Bad/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace Aula12.Bad
+{
+    public interface IEmployee
+    {
+        void CalculateSalary();
+        void CalculateBenefits();
+    }
+
+    public class FullTimeEmployee : IEmployee
+    {
+        public void CalculateBenefits()
+        {
+            Console.WriteLine("Calculate benefits.");
+        }
+
+        public void CalculateSalary()
+        {
+            Console.WriteLine("Calculate salary.");
+        }
+    }
+
+    public class ContractEmployee : IEmployee
+    {
+        public void CalculateBenefits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateSalary()
+        {
+            Console.WriteLine("Calculate salary.");
+        }
+    }
+}
---
+++ C Sharp/Aula12/Good/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace Aula12.Good
+{
+    public interface ISalaryCalculator
+    {
+        void CalculateSalary();
+    }
+
+    public interface IBenefitsCalculator
+    {
+        void CalculateBenefits();
+    }
+
+    public class FullTimeEmployee : ISalaryCalculator, IBenefitsCalculator
+    {
+        public void CalculateBenefits()
+        {
+            Console.WriteLine("Calculate benefits.");
+        }
+
+        public void CalculateSalary()
+        {
+            Console.WriteLine("Calculate salary.");
+        }
+    }
+
+    public class ContractEmployee : ISalaryCalculator
+    {
+        public void CalculateSalary()
+        {
+            Console.WriteLine("Calculate salary.");
+        }
+    }
+}
